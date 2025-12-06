de667f6bd1f864e056af676712b9f362c0e70036
mryule1401 <lehuy011406@gmail.com>
2025-12-06 16:07:29 +1030

    login

--- my-react-ts-app/src/route/routes.tsx
+++ my-react-ts-app/src/route/routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage"
+import LoginPage from "../login/LoginPage";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />, 
+  },
+    {
+    path: "/login",
+    element: <LoginPage/>, 
   },
 ]);
 
